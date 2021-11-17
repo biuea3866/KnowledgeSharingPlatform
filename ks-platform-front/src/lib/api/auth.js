b729570eded86c85f740e2edd324de39c7bf5f9f
biuea <biuea3866@gmail.com>
2021-11-17 16:04:46 +0900

    #16 saveUser

--- ks-platform-front/src/lib/api/auth.js
+++ ks-platform-front/src/lib/api/auth.js
@@ -8,11 +8,11 @@ export const login = ({
     password
 }, { 
     withCredentails: true 
-}).then((Response) => {
-    console.log(Response.headers['set-cookie']);
-    console.log(Response.headers['set-cookie']['token']);
+}).then(function(response) {
+    console.log(response.headers['set-cookie']);
+    console.log(response.headers['set-cookie']['token']);
 
-    localStorage.setItem('token', Response.headers['set-cookie']['token']);
+    localStorage.setItem('token', response.headers['set-cookie']['token']);
 });
 
 export const getUser = () => client.get('http://18.177.184.216/auth/get-user', {
